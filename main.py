8640b108c3b05b5acd97a8ce6f86fd63110dc4bc
varun sai <varun@idealabs.fyi>
2025-08-28 11:50:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -205,5 +205,6 @@ def health_check():
         }
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+     import uvicorn
+     port = int(os.getenv("PORT", 8000))
+     uvicorn.run(app, host="0.0.0.0", port=port)
